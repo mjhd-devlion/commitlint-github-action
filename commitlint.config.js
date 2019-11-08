fc51d90329dc583fdbd41be292313f3d12c29b89
Yusuke Otsuka <otsuka_yusuke@cyberagent.co.jp>
2019-11-08 14:50:22 +0900

    wip

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   defaultIgnores: false,
-  ignores: [_ => true],
+  ignores: [_ => false],
   rules: {
     'body-leading-blank': [2, 'always'],
     'body-max-line-length': [2, 'always', 72],
