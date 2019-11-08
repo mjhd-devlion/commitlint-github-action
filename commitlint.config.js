efb26b6254fdc26441dc72abce28d8c3124833a9
Yusuke Otsuka <otsuka_yusuke@cyberagent.co.jp>
2019-11-08 09:59:42 +0900

    test: check default ignore

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,47 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  defaultIgnores: false,
+  ignores: [
+    c =>
+      c.match(
+        /^((Merge pull request)|(Merge (.*?) into (.*?)|(Merge branch (.*?)))(?:\r?\n)*$)/m,
+      ),
+    c => c.match(/^(R|r)evert (.*)/),
+    c => c.match(/^Merged (.*?)(in|into) (.*)/),
+    c => c.match(/^Merge remote-tracking branch (.*)/),
+    c => c.match(/^Automatic merge(.*)/),
+    c => c.match(/^Auto-merged (.*?) into (.*)/),
+  ],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 72],
+    'subject-max-length': [1, 'always', 50],
+    'subject-empty': [2, 'never'],
+    'scope-empty': [2, 'always'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'fix',
+        'hotfix',
+        'feat',
+        'update',
+        'refactor',
+        'clean',
+        'disable',
+        'revert',
+        'upgrade',
+        'docs',
+        'style',
+        'chore',
+        'perf',
+        'a11y',
+        'test',
+        'release',
+      ],
+    ],
+  },
 }
